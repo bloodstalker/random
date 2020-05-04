3db176dd58b06be623b7b8b7db5a8990ddd9a1cc
bloodstalker <thabogre@gmail.com>
2020-05-04 20:07:48 +0430

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # random
-random
+A collection of random python scripts.
+
+## Tracer
+You put in a comment with the keyword `@trace` plus a requirement ID the source code in that location is implementing and tracer will generate a CSV list of locations, function names and the requirement IDs. Basically a poor man's requirements traceability generator.
+
+## Degger
+...
--- degger/degger.py
+++ degger/degger.py
@@ -47,92 +47,31 @@ class Argparser(object):
         parser.add_argument("--dbg", action="store_true", help="debug", default=False)
         self.args = parser.parse_args()
 
-
-def find_typerefs(node, typename):
-    if node.kind.is_reference():
-        if node.spelling == "class std::" + typename:
-            print("Found %s [Line=%s, Col=%s]"%(typename, node.location.line, node.location.column))
-    for c in node.get_children():
-        find_typerefs(c, typename)
-
-
-def getParentFunctionDeclIfAny(cursor):
-    if cursor.semantic_parent.kind == None:
-        return None
-    elif cursor.semantic_parent.kind == clang.cindex.CursorKind.FUNCTION_DECL:
-        return cursor.semantic_parent
-    else:
-        getParentFunctionDecl(cursor.semantic_parent)
-
-
-def getReqIDIfAny(raw_comment):
-    reqIDRegEx = "\s([1-9](\.[1-9])*?)\s"
-    return re.findall(reqIDRegEx, raw_comment)
-
-
-def generateOutput(traceability_list, argparser):
-    if argparser.args.outtype == "csv":
-        fs = argparser.args.fs
-        for elem in traceability_list:
-            dump = str()
-            for subelem in elem:
-                dump += subelem + fs
-            print(dump)
-
-def getListAsString(traceability_list, argparser):
-    fs = argparser.args.fs
-    dump = str()
-    for elem in traceability_list:
-        for subelem in elem:
-            dump += subelem + fs
-        dump += "\n"
-    return dump
-
-
-def getcompbase(source_file_path, compbase_path):
-    compdb = clang.cindex.CompilationDatabase.fromDirectory(compbase_path)
-    compcomms = compdb.getCompileCommands(source_file_path)
-    for comm in compcomms:
-        print(comm.arguments)
-        print(comm.directory)
-        print(comm.filename)
-    return compcomms
+def get_function_definitions(node):
+    if node.kind.FUNCTION_DECL and node.is_definition():
+        if "makejmptable" == node.spelling:
+            #print(node.kind)
+            print(node.extent)
+            for arg in node.get_arguments():
+                if arg.type.kind == clang.cindex.TypeKind.POINTER:
+                    #print(arg.spelling)
+                    #print(arg.type.spelling)
+                    print("\\valid("+arg.spelling+")")
+            for c_node in node.get_children():
+                print(c_node.spelling)
+    for c_node in node.get_children():
+        get_function_definitions(c_node)
 
 
 # write code here
 def premain(argparser):
     signal.signal(signal.SIGINT, SigHandler_SIGINT)
     #here
-    traceability_list = []
-    traceability_list.append(["Requirement ID", "Function name", "Function line", "Function column",
-        "Function file", "comment line", "comment column", "comment file"])
     if argparser.args.file is not None:
         for src_file in argparser.args.file:
-            compile_command = getcompbase(src_file, argparser.args.compdb)
             index = clang.cindex.Index.create()
             tu = index.parse(src_file)
-            for token in tu.cursor.get_tokens():
-                if token.kind == clang.cindex.TokenKind.COMMENT:
-                    if token.spelling.find("@trace") != -1:
-                        src_range = token.extent
-                        cursor = token.cursor
-                        declFunc = getParentFunctionDeclIfAny(token.cursor)
-                        if declFunc.spelling != None:
-                            req_id = token.spelling.strip()
-                            req_id = req_id.strip("/")
-                            req_id = req_id.strip("*")
-                            req_id = req_id[7:]
-                            req_id = req_id.strip()
-                            traceability_list.append([req_id, declFunc.spelling, repr(declFunc.location.line),
-                                repr(declFunc.location.column), repr(declFunc.location.file), repr(token.location.line),
-                                repr(token.location.column), repr(token.location.file)])
-    else:
-        print("you have to specify the name of the file...")
-
-    if len(traceability_list) > 0:
-        generateOutput(traceability_list, argparser)
-        out_file = open(src_file+".trace.txt", "w")
-        out_file.write(getListAsString(traceability_list, argparser))
+            get_function_definitions(tu.cursor)
 
 
 def main():
