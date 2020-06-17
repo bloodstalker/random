051b28eb51c499d42d7ce59a6f2d7cfcdb301ec9
bloodstalker <thabogre@gmail.com>
2020-06-17 20:01:49 +0430

    finished

--- mdchecker/mdchecker.js
+++ mdchecker/mdchecker.js
@@ -1,20 +1,31 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 //npm install argparse
-//npm install markdown-it
 //npm install jsdom
+//npm install markdown-it
+//npm install markdown-it-texmath
+//npm install katex
+//npm install markdown-it-multimd-table
 "use strict"
 const fs = require("fs")
 const jsdom = require("jsdom")
 const {JSDOM} = jsdom;
 
-const mit = require("markdown-it")
-const mt = new mit()
+const mitTexMath = require("markdown-it-texmath")
+const mitMulMd = require("markdown-it-multimd-table")
+const mit = require("markdown-it")({html:true})
+  .enable(["table"])
+  .disable(["strikethrough"])
+  .use(mitTexMath, {
+    engine: require("katex"), 
+    delimiters: "gitlab", 
+    katexOptions: {macros: {"\\RR":"\\mathbb{R}"}}})
+  .use(mitMulMd)
 
 const ArgumentParser = require("argparse").ArgumentParser
 const parser = new ArgumentParser({})
 
 parser.addArgument(["--xml"], {
-  help: "path to xml file",
+  help: "path to the xml file",
   required: true,
   dest: "xmlPath"
 })
@@ -26,13 +37,18 @@ function mdchecker (){
     contentType:"text/xml"
   })
   const xmlSerialized = dom.serialize()
-  const r = /\<\!\[CDATA\[(?<cdata>.*)\]\]\>/g
+  const r = /<(?<parent>.*)>\<\!\[CDATA\[(?<cdata>(.|\n?|(\r\n)?)*?)\]\]\>/g
   const matches = xmlSerialized.matchAll(r)
   for (let match of matches){
-    let cdata = match.groups
-    console.log(cdata)
+    let {parent, cdata} = match.groups
+    if (("Description" != parent) && ("Postcondition" != parent) && ("Precondition" != parent) && ("Remark" != parent)) {
+      console.log("CDATA appears in illegal node type.")
+      process.exit(1)
+    } else {
+      const res = mit.render(cdata)
+      console.log(res)
+    }
   }
-
 }
 
 mdchecker()
