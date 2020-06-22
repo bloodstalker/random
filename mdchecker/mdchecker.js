1d6876ad98ae9526fe0b2eb18dc01ca005e78973
bloodstalker <thabogre@gmail.com>
2020-06-22 21:24:09 +0430

    update

--- mdchecker/mdchecker.js
+++ mdchecker/mdchecker.js
@@ -28,17 +28,25 @@ function mdchecker (){
   const args = parser.parseArgs()
   const txt = fs.readFileSync(args.xmlPath, "utf-8")
   const dom = new JSDOM(txt, {
-    contentType:"text/xml"
+    contentType:"text/xml",
   })
+
+  const domParser = new dom.window.DOMParser
+  const doc = domParser.parseFromString(txt, 'text/xml')
+  const qResult = doc.querySelectorAll("Method").textContent
+  //console.log(doc.childNodes)
+  //console.log(qResult)
+
   const xmlSerialized = dom.serialize()
-  const r = /<(?<parent>.*)>\<\!\[CDATA\[(?<cdata>(.|\n?|(\r\n)?)*?)\]\]\>/g
+  const r = /\<Method\>(\s|\n)*?\<Identifier\>(?<identifier>.*)\<\/Identifier\>(.|\n)*?<(?<parent>.*)>(\s|\n)*\<\!\[CDATA\[(?<cdata>(.|\n?|(\r\n)?)*?)\]\]\>/g
   const matches = xmlSerialized.matchAll(r)
   for (let match of matches){
-    let {parent, cdata} = match.groups
+    let {identifier, parent, cdata} = match.groups
     if (("Description" != parent) && ("Postcondition" != parent) && ("Precondition" != parent) && ("Remark" != parent)) {
       console.log("CDATA appears in illegal node type.")
       process.exit(1)
     } else {
+      console.log(identifier)
       const res = mit.render(cdata)
       console.log(res)
     }
